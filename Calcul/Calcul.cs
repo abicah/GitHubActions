26448cad863e40cb7e4a60dd9a02dafed46f52bb
rani <rb@wisecom.fr>
2024-12-08 19:47:57 +0100

    add sub

--- Calcul/Calcul.cs
+++ Calcul/Calcul.cs
@@ -2,7 +2,9 @@ public class Calcul
 {
     public int Add(int a,int b)=>a+b;
 
-    public int Multiply(int a,int b)=>a*b;
+    public int Mult(int a,int b)=>a*b;
+
+    public int Sub(int a,int b)=>a-b;
 
 
 }
--- CalculTests/CalculTests.cs
+++ CalculTests/CalculTests.cs
@@ -14,20 +14,33 @@ public class CalculTests
         //Act
          int actual= c.Add(a,b);
          //Arrange
-         actual.Should().Be(3);
+         actual.Should().Be(expected);
+
+    }
+
+    [Theory]
+    [InlineData(2, 1, 1)]
+    public void Sub_TwoNumbers_ReturnsSubstraction(int a,int b,int expected)
+    {
+        //Assert
+         Calcul c=new();
+        //Act
+         int actual= c.Sub(a,b);
+         //Arrange
+         actual.Should().Be(expected);
 
     }
 
      [Theory]
     [InlineData(2, 3, 6)]
-    public void Multiply_TwoNumbers_ReturnsProduct(int a,int b,int expected)
+    public void Mult_TwoNumbers_ReturnsProduct(int a,int b,int expected)
     {
         //Assert
          Calcul c=new();
         //Act
-         int actual= c.Multiply(a,b);
+         int actual= c.Mult(a,b);
          //Arrange
-         actual.Should().Be(6);
+         actual.Should().Be(expected);
 
     }
 
